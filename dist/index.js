382f556a7492033ceab7d6b7606f5e8922b4ea19
Jaival <pateljaival05@gmail.com>
2023-07-31 11:48:13 -0400

    basic file commands

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,16 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const figlet = require("figlet");
+const fs = require('fs');
+const path = require('path');
 const { Command } = require('commander');
 const program = new Command();
 console.log(figlet.textSync("FileMan"));
@@ -11,4 +22,43 @@ program
     .option("-t, --touch <value>", "Create a file")
     .parse(process.argv);
 const options = program.opts();
+function listDirContents(filepath) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const files = yield fs.promises.readdir(filepath);
+            console.log("Files: ", files);
+            const detailedFilesPromises = files.map((file) => __awaiter(this, void 0, void 0, function* () {
+                let fileDetails = yield fs.promises.lstat(path.resolve(filepath, file));
+                console.log("File Details: ", fileDetails);
+                const { size, birthtime } = fileDetails;
+                return { filename: file, "size(KB)": size, created_at: birthtime };
+            }));
+            const detailedFailes = yield Promise.all(detailedFilesPromises);
+            console.table(detailedFailes);
+        }
+        catch (err) {
+            console.log("Error Occured while reading the directory: ", err);
+        }
+    });
+}
+function createDir(filepath) {
+    if (!fs.existsSync(filepath)) {
+        fs.mkdirSync(filepath);
+        console.log("The Directory has been made successfully");
+    }
+}
+function createFile(filepath) {
+    fs.openSync(filepath, 'w');
+    console.log("An empty file is created");
+}
+if (options.ls) {
+    const filePath = typeof options.ls === 'string' ? options.ls : __dirname;
+    listDirContents(filePath);
+}
+if (options.mkdir) {
+    createDir(path.resolve(__dirname, options.mkdir));
+}
+if (options.touch) {
+    createFile(path.resolve(__dirname, options.touch));
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -15,16 +15,56 @@ program
   .option("-t, --touch <value>", "Create a file")
   .parse(process.argv);
 
+  const options = program.opts();
+
 
 async function listDirContents(filepath: string) {
   try {
+    const files = await fs.promises.readdir(filepath)
+    console.log("Files: ", files)
+
+    const detailedFilesPromises = files.map(async (file: string) => {
+      let fileDetails = await fs.promises.lstat(path.resolve(filepath, file))
+      
+      const {size, birthtime} = fileDetails
+      return {filename: file, "size(KB)": size, created_at: birthtime}
+    })
+
+    const detailedFailes = await Promise.all(detailedFilesPromises)
+    console.table(detailedFailes)
 
   } catch (err) {
     console.log("Error Occured while reading the directory: ", err)
   }
 }
 
-const options = program.opts();
+function createDir(filepath: string) {
+  if (!fs.existsSync(filepath)) {
+    fs.mkdirSync(filepath)
+    console.log("The Directory has been made successfully")
+  }
+}
+
+function createFile(filepath: string) {
+  fs.openSync(filepath, 'w')
+  console.log("An empty file is created")
+
+}
+
+if (options.ls) {
+  const filePath = typeof options.ls === 'string' ? options.ls: __dirname;
+  listDirContents(filePath)
+}
+
+if (options.mkdir) {
+  createDir(path.resolve(__dirname, options.mkdir))
+}
+
+if (options.touch) {
+  createFile(path.resolve(__dirname, options.touch))
+}
+
+
 
 
 
